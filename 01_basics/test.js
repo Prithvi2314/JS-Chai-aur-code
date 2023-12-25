f4e553e84b5fda44afed89a88980da89eb2d36d8
Prithvi Kamble <kambleprithvi2300@gmail.com>
2023-12-25 20:01:09 +0530

    Test

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,5 +1,1 @@
-console.log("I love you baccha! cholly bol chal motiii");
-console.log("143");
-console.log("tu mere ko galat samajhti hai😒");
-console.log("hi");
-console.log("hello");
+console.log("hello world")
