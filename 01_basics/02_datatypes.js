9ca76df439d81d957eef1aec6c99c99408af842c
Prithvi Kamble <kambleprithvi2300@gmail.com>
2023-12-27 20:58:29 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict";// treat all JS code as newer version
+
+//alert( 3 + 3)// we  are using nodejs , not broswer
+
+console.log( 3 
+    +
+     3) // code readability should be high 
+
+console.log("Prithvi") 
+
+
+let name = "prithvi"
+let age = 22
+let isLoggedIn = false
+
+
+// number =>2 to power 53
+// bigint =>(if number is 2 big it is basically used in trading stockmarket ,facebook,reddit ect)
+// string =>""
+// boolean => true/false
+// null =>(stand alone value bhi hai ye)
+// undefined =>
+// symbol => uniquness k liye use hota hai
+
+
+//object
+
+console.log(typeof "Prithvi")
+console.log(typeof 22)
+console.log(typeof false)
+console.log(typeof age)
+console.log(typeof null)
+
+
+
