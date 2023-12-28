656f696b3a526018db3008a950b3c41b348c4f9d
Prithvi Kamble <kambleprithvi2300@gmail.com>
2023-12-28 18:02:13 +0530

    conversionof datatypes

---
+++ 01_basics/03_converstionOperations.js
@@ -0,0 +1,28 @@
+let score = "prithvi"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => dalne pe 33 hi ayega 
+// "33abc" => NaN (NOT A NUMBER) 
+// true => 1; false => 0
+
+let isLoggedIn = "prithvi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false (ha tho boolean jaise apn ko malum hai true or false ka valse deta) 
+// "" => false
+// "prithvi" => true
+
+let someNumber = 33
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log( typeof  stringNumber);
