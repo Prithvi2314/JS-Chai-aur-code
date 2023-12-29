38c7c49b930d74eea2a7dfd1f852be54e8433474
Prithvi Kamble <kambleprithvi2300@gmail.com>
2023-12-29 21:09:37 +0530

    change in datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+//   Primitive
+
+// 7 types : String, Number, Boolean, Null, undefined, symbol, BigInt
+
+const score = 198
+const scoreValue = 234.5
+
+const isLoggedIn = false
+const oustsideTemp = null
+let userEmail;
+
+const id = Symbol ('2000')
+const anotherId = Symbol ('2000')
+
+console.log(id === anotherId);
+
+const  bigNumber =  1593248765212564878254114n
+
+
+// Reference ( non primitive)
+
+// Array, objects, Functions
+
+
+const heros = ["shaktiman","shinchan","perman"];
+ let myObj = {
+    name :"prithvi",
+    age : 22,
+}
+
+const myfunction = function (){
+    console.log("hello world");
+}
+
+console.log(typeof bigNumber);
