1fe9cbe5220b97ec81bc94d0f0f49c6a57e4c6af
Prithvi Kamble <kambleprithvi2300@gmail.com>
2024-01-09 18:58:36 +0530

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+ const score = 108
+ // console.log(score);
+
+ const  balance = new Number (100)
+ // console.log(balance);
+
+ // console.log(balance.toString().length);
+ // console.log(balance.toFixed(2));
+
+ const otherNumber = 1123.8714
+ // console.log(otherNumber.toPrecision(3));
+
+ const hundreds = 1000000
+ // console.log(hundreds.toLocaleString('en-IN'));
+
+ //++++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++
+
+
+ // console.log(Math);
+ // console.log(Math.abs(-9));
+ // console.log(Math.round(2.6)); 
+ // console.log(Math.ceil(5.3));
+ // console.log(Math.floor(5.9));
+ // console.log(Math.min(9, 4, 3, 7));
+ // console.log(Math.max(9, 4, 3, 7));
+ 
+
+ // console.log(Math.random());
+ // console.log((Math.random()*10) + 1);
+ // console.log(Math.floor(Math.random()*10) + 1);
+
+ const min = 10
+ const max = 20 
+
+console.log ( Math.floor (Math.random() * (max - min + 1)) + min) 
+
