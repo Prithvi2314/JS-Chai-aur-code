7638d4b54071320858fc0bd4b0a9530f95bb1f3e
Prithvi Kamble <kambleprithvi2300@gmail.com>
2024-01-23 12:26:49 +0530

    newly added arrays in function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -41,11 +41,45 @@ function loginUserMessage (username = " sam "){
 
 // console.log(loginUserMessage("prithvi"));
 
-console.log(loginUserMessage("prithvi"));
+// console.log(loginUserMessage("prithvi"));
 
 /*
 agar uper apn sam ko declare kar rahe hai user mai tho vo if ki condition ko nhi excute karega or sam dega butnich agr apn console kar rahe hai tho vo phir niche di gayi value ko hi use kar k excute karega 
 */
+
+
+function calculatecartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculatecartPrice(200,500,300,6000))
+
+const user = {
+    username : "Prithvi",
+    prices : 199
+}
+
+function handleobject(anyobject){
+     console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleobject(user)
+
+handleobject({
+    username : "sam",
+    price : 299
+})
+
+const myNewArray = [ 200, 300, 600, 4000]
+
+function returnSecondValue(myarray){
+    return myarray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200, 300, 600, 4000]));
+
 
 
 
