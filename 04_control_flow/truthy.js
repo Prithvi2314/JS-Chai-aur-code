31b38c4825d0837246b68e66152c858e969303f9
Prithvi Kamble <kambleprithvi2300@gmail.com>
2024-02-16 23:25:36 +0530

    truthy and falsy

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -39,6 +39,4 @@ console.log(val1);
 // condition ? true : false 
 
 const iceTeaprice = 100
-iceTeaprice <= 80 ? console.log("less than 80") : console.log("more than 80");
-
-
+iceTeaprice <= 80 ? console.log("less than 80") : console.log("more than 80")
