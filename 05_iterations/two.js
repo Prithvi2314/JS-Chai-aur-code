8064b072501de4c790e4f2736c056bae633cb940
Prithvi Kamble <kambleprithvi2300@gmail.com>
2024-02-17 22:11:44 +0530

    do while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of the index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ["Naruto" , "shinchan", "tom and jerry"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(` My Fav is ${myArray[arr]}`);
+    arr = arr + 1 
+}
+
+let marks = -10
+
+do {
+    console.log(` Total marks are ${marks}`);
+    marks++
+} while (marks <= 10);
