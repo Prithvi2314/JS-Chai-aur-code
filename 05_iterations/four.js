b72b2d2fb2429d555f9ef65707c3b0b9502c4159
Prithvi Kamble <kambleprithvi2300@gmail.com>
2024-02-18 23:50:55 +0530

    for in loop and use of map in it

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+const myObject = {
+    js: "javascript",
+    cpp: "c++",
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+
+// const map = new Map ()
+// map.set('IN' , "India")
+// map.set('FR' , "France")
+// map.set('GRE' , "germany")
+
+// for (const key in map) {
+//    console.log(key);
+// }
+
+// for in loop use hua hai ismai
+
